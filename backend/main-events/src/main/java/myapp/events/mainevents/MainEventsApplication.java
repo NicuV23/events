908a448ddc38fc7a785fe035d982e159c5104580
NicuV23 <v.nicu213@gmail.com>
2025-03-04 19:19:00 +0200

    Initialize backend structure with Maven; add application properties, .gitignore, and IDE configuration files

---
+++ backend/main-events/src/main/java/myapp/events/mainevents/MainEventsApplication.java
@@ -0,0 +1,13 @@
+package myapp.events.mainevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainEventsApplication {
+
+    public static void main(String[] args) {
+SpringApplication.run(MainEventsApplication.class, args);
+    }
+}
---
+++ backend/main-events/src/test/java/myapp/events/mainevents/MainEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package myapp.events.mainevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
